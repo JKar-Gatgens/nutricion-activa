6aeccc72b621f7bb593f60cfdde2845c0d2499a9
Johan Gatgens <jgatgens40325@ufide.ac.cr>
2026-07-10 11:37:57 -0600

    Sprint 0: proyecto base Spring Boot con landing de construccion

---
+++ src/main/java/com/nutricionactiva/NutricionActivaApplication.java
@@ -0,0 +1,14 @@
+package com.nutricionactiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutricionActivaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutricionActivaApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/nutricionactiva/NutricionActivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutricionactiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutricionActivaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
